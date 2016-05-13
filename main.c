f1b302fb7c4128c841976d0de1a103d965e03214
zloedi <stoiko.todorov@gmail.com>
2016-05-13 15:17:30 +0300

    make zhost compile and run

--- main.c
+++ main.c
@@ -1,5 +1,92 @@
 #include "host.h"
 
+static void X_AtExit( void ) {
+    // End block before the renderer is killed
+    CON_End();
+
+    R_Done();
+    VAR_Done();
+    CMD_Done();
+    CON_Done();
+    SYS_Done();
+    A_Done();
+    SDL_Quit();
+}
+
+static void X_ProcessKey( int code, bool_t down ) {
+	// try the console
+	if ( CON_OnKeyboard( code, down ) ) {
+		return;
+    }
+	// GUI widgets
+	//if ( WG_OnKeyboard( code, ch, down ) )
+	//	return;
+	// application specific callback
+	//if ( sys_onKeyCallback( code, ch, down) )
+	//	return;
+	// command key bindings
+	//K_OnKeyboard( code, down );
+}
+
+static bool_t X_ProcessEvents( void ) {
+    bool_t quit = false;
+    SDL_Event event;
+    while ( SDL_PollEvent( &event ) ) {
+        int code = event.key.keysym.sym;
+        static bool_t laltDown, raltDown, ctlDown;
+        switch (event.type) {
+            case SDL_TEXTINPUT:
+                CON_OnText( event.text.text );
+                break;
+
+            case SDL_KEYDOWN:
+                if ( code == SDLK_LCTRL || code == SDLK_RCTRL ) {
+                    ctlDown = true;
+                } else if ( code == SDLK_LALT ) {
+                    laltDown = true;
+                } else if ( code == SDLK_RALT ) {
+                    raltDown = true;
+                }
+                // quit on alt + F4 is hardcoded
+                if ( ( raltDown || laltDown ) && code == SDLK_F4 ) {
+                    quit = true;
+                }
+                // console toggle is hardcoded
+                else if ( code == SDLK_BACKQUOTE ) {
+                    CON_Toggle( ctlDown );
+                } 
+                else {
+                    X_ProcessKey( code, true );
+                }
+                break;
+
+            case SDL_KEYUP:
+                if ( code == SDLK_LCTRL || code == SDLK_RCTRL ) {
+                    ctlDown = false;
+                } else if ( code == SDLK_LALT ) {
+                    laltDown = false;
+                } else if ( code == SDLK_RALT ) {
+                    raltDown = false;
+                } 
+                // printscreen is hardcoded
+                if ( code == SDLK_PRINTSCREEN || code == SDLK_SYSREQ ) {
+                    R_SaveScreenshot();
+                } 
+                else {
+                    X_ProcessKey( code, false );
+                }
+                break;
+
+            case SDL_QUIT:
+                quit = true;
+                break;
+
+            default:;
+        }
+    }
+    return quit;
+}
+
 int main( int argc, char *argv[] ) {
     if ( SDL_InitSubSystem( SDL_INIT_GAMECONTROLLER | SDL_INIT_TIMER | SDL_INIT_AUDIO ) < 0 ) {
         SYS_ErrorBox( "main: SDL could not initialize! SDL Error: %s", SDL_GetError() );
