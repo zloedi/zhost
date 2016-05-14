b03cc7199f312d785c6c7dcc7aaf6f012670b754
zloedi <stoiko.todorov@gmail.com>
2016-05-14 10:16:05 +0300

    retabbed sys.c

--- sys.c
+++ sys.c
@@ -5,11 +5,11 @@ static char *sys_prefsDir;
 
 void SYS_ErrorBox( const char *fmt, ... ) {
     char buf[VA_SIZE] = {0};
-	va_list argptr;
-	va_start (argptr, fmt);
-	vsnprintf (buf, VA_SIZE, fmt, argptr);
-	va_end (argptr);
-	buf[VA_SIZE - 1] = '\0';
+    va_list argptr;
+    va_start (argptr, fmt);
+    vsnprintf (buf, VA_SIZE, fmt, argptr);
+    va_end (argptr);
+    buf[VA_SIZE - 1] = '\0';
     fprintf( stderr, "ERROR: %s\n", buf );
     fflush( stderr );
     SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_WARNING, "Error!", buf, NULL );
@@ -17,7 +17,7 @@ void SYS_ErrorBox( const char *fmt, ... ) {
 }
 
 static timestamp_t SYS_Milliseconds( void ) {
-	return ( timestamp_t )SDL_GetTicks();
+    return ( timestamp_t )SDL_GetTicks();
 }
 
 void SYS_SampleTime( void ) {
@@ -54,60 +54,60 @@ void SYS_Done( void ) {
 #include <dirent.h>
 
 static int cmpr(const void *a, const void *b) { 
-	return stricmp(*(char**)a, *(char**)b);
+    return stricmp(*(char**)a, *(char**)b);
 }
 
 char** SYS_ListFiles( const char *dir, const char *pattern ) {
-	struct dirent *de;
-	DIR           *d;
-	char          **list;
-	char          buf[VA_SIZE];
+    struct dirent *de;
+    DIR           *d;
+    char          **list;
+    char          buf[VA_SIZE];
 
-	d = opendir( dir );
-	if ( ! d ) {
-		list = A_MallocZero( sizeof( *list ) );
-		return list;
-	}
+    d = opendir( dir );
+    if ( ! d ) {
+        list = A_MallocZero( sizeof( *list ) );
+        return list;
+    }
 
-	size_t count = 0;
-	while( ( de = readdir( d ) ) ) {
-		if ( de->d_name[0] == '.' )
-			continue;
-		
-		if ( COM_Match( "*~", de->d_name, true ) )
-			continue;
-		
-		if ( COM_Match( pattern, de->d_name, false ) ) {
-			count++;
-		}
-	}
+    size_t count = 0;
+    while( ( de = readdir( d ) ) ) {
+        if ( de->d_name[0] == '.' )
+            continue;
+        
+        if ( COM_Match( "*~", de->d_name, true ) )
+            continue;
+        
+        if ( COM_Match( pattern, de->d_name, false ) ) {
+            count++;
+        }
+    }
 
-	rewinddir( d );
-	
-	list = A_Malloc( ( count + 1 ) * sizeof( *list ) );
+    rewinddir( d );
+    
+    list = A_Malloc( ( count + 1 ) * sizeof( *list ) );
 
-	count = 0;
-	while( ( de = readdir( d ) ) ) {
-		if ( de->d_name[0] == '.' )
-			continue;
-		
-		if ( COM_Match( "*~", de->d_name, true ) )
-			continue;
-		
-		if ( COM_Match( pattern, de->d_name, false ) ) {
-			if ( de->d_type == DT_DIR ) {
-				COM_Sprintf( buf, VA_SIZE, "%s%s/", dir, de->d_name );
-			} else {
-				COM_Sprintf( buf, VA_SIZE, "%s%s", dir, de->d_name );
-			}
-			list[count] = A_StrDup( buf );
-			count++;
-		}
-	}
-	qsort( list, count, sizeof( *list ), cmpr );
-	list[count] = NULL;
-	closedir( d );
-	return list;
+    count = 0;
+    while( ( de = readdir( d ) ) ) {
+        if ( de->d_name[0] == '.' )
+            continue;
+        
+        if ( COM_Match( "*~", de->d_name, true ) )
+            continue;
+        
+        if ( COM_Match( pattern, de->d_name, false ) ) {
+            if ( de->d_type == DT_DIR ) {
+                COM_Sprintf( buf, VA_SIZE, "%s%s/", dir, de->d_name );
+            } else {
+                COM_Sprintf( buf, VA_SIZE, "%s%s", dir, de->d_name );
+            }
+            list[count] = A_StrDup( buf );
+            count++;
+        }
+    }
+    qsort( list, count, sizeof( *list ), cmpr );
+    list[count] = NULL;
+    closedir( d );
+    return list;
 }
 
 #elif defined(__WIN32__)
@@ -116,62 +116,62 @@ char** SYS_ListFiles( const char *dir, const char *pattern ) {
 #include <windows.h>
 
 char** SYS_ListFiles( const char *dir, const char *pattern ) {
-	char **list = NULL;
-	WIN32_FIND_DATA fd;
-	HANDLE h;
-	size_t count;
+    char **list = NULL;
+    WIN32_FIND_DATA fd;
+    HANDLE h;
+    size_t count;
 
-	h = FindFirstFile( va( "%s*", dir ), &fd );
-	if ( h == INVALID_HANDLE_VALUE ) {
-		list = A_MallocZero( sizeof( *list ) );
-		return list;
-	}
+    h = FindFirstFile( va( "%s*", dir ), &fd );
+    if ( h == INVALID_HANDLE_VALUE ) {
+        list = A_MallocZero( sizeof( *list ) );
+        return list;
+    }
 
-	count = 1;
-	while ( FindNextFile( h, &fd ) != 0 ) {
-		if ( fd.cFileName[0] == '.' )
-			continue;
-		
-		if ( COM_Match( "*~", fd.cFileName, true ) )
-			continue;
+    count = 1;
+    while ( FindNextFile( h, &fd ) != 0 ) {
+        if ( fd.cFileName[0] == '.' )
+            continue;
+        
+        if ( COM_Match( "*~", fd.cFileName, true ) )
+            continue;
 
-		if ( COM_Match( pattern, fd.cFileName, false ) ) {
-			count++;
-		}
-	}
+        if ( COM_Match( pattern, fd.cFileName, false ) ) {
+            count++;
+        }
+    }
 
-	list = A_MallocZero( ( count + 1 ) * sizeof( *list ) );
-	list[count] = NULL;
+    list = A_MallocZero( ( count + 1 ) * sizeof( *list ) );
+    list[count] = NULL;
 
-	h = FindFirstFile( va( "%s/*", dir ), &fd );
-	if ( h == INVALID_HANDLE_VALUE ) {
-		return list;
-	}
+    h = FindFirstFile( va( "%s/*", dir ), &fd );
+    if ( h == INVALID_HANDLE_VALUE ) {
+        return list;
+    }
 
-	count = 0;
-	do {
-		char *buf;
+    count = 0;
+    do {
+        char *buf;
 
-		if ( fd.cFileName[0] == '.' )
-			continue;
-		
-		if ( COM_Match( "*~", fd.cFileName, true ) )
-			continue;
+        if ( fd.cFileName[0] == '.' )
+            continue;
+        
+        if ( COM_Match( "*~", fd.cFileName, true ) )
+            continue;
 
-		if ( ! COM_Match( pattern, fd.cFileName, false ) ) {
-			continue;
-		}
+        if ( ! COM_Match( pattern, fd.cFileName, false ) ) {
+            continue;
+        }
 
-		if ( fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) {
-			buf = va( "%s%s/", dir, fd.cFileName );
-		} else {
-			buf = va( "%s%s", dir, fd.cFileName );
-		}
+        if ( fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) {
+            buf = va( "%s%s/", dir, fd.cFileName );
+        } else {
+            buf = va( "%s%s", dir, fd.cFileName );
+        }
 
-		list[count++] = A_StrDup( buf );
-	} while ( FindNextFile( h, &fd ) != 0 );
+        list[count++] = A_StrDup( buf );
+    } while ( FindNextFile( h, &fd ) != 0 );
 
-	return list;
+    return list;
 }
 
 #else
