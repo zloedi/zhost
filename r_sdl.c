9a3a44d9ef5e55c084a7dd3f454da30c31911723
stoiko <stoiko.todorov@gmail.com>
2016-05-28 20:42:38 +0300

    added stb_image

--- r_sdl.c
+++ r_sdl.c
@@ -1,4 +1,6 @@
 #include "host.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 static varPtr_t r_windowWidth;
 static varPtr_t r_windowHeight;
@@ -124,6 +126,19 @@ void R_Done() {
     SDL_DestroyRenderer( r_renderer );
     SDL_DestroyWindow( r_window );
     CON_Printf( "Renderer done.\n" );
+}
+
+int R_LoadTexture( const char *pathToImage ) {
+    int x,y,n;
+    unsigned char *data = stbi_load(pathToImage, &x, &y, &n, 0);
+    // ... process data if not NULL ...
+    // ... x = width, y = height, n = # 8-bit components per pixel ...
+    // ... replace '0' with '1'..'4' to force that many components per pixel
+    // ... but 'n' will always be the number that it would have been if you said 0
+    if ( data ) {
+    }
+    stbi_image_free( data );
+    return 0;
 }
 
 int RI_CreateStaticTexture( const byte *data, int width, int height, riFlags_t flags, int bytesPerPixel ) {
